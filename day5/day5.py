98b6ebcb17e263b8ba94a5dfffd352a50e2a6c0d
George Yao <gyao@andrew.cmu.edu>
2022-12-05 18:36:41 -0500

    day5 solution and modifications to util function.

---
+++ day5/day5.py
@@ -0,0 +1,36 @@
+from utility import parseInput, parseInputDay5
+import itertools
+from typing import List
+
+
+class Solution:
+
+    def stack_crate_operator(self, use_real_input: bool = False, second_problem: bool = False) -> str:
+        """
+        Returns a str of the top letters of each stack of boxes
+        :param use_real_input: bool. Use real data if True, else test data
+        :param second_problem: bool. If True then return answer for second part
+        :return: str. Str containing the letters of the top of each stack
+        """
+        res = ""
+        # O(N) time, O(N) space where N is the number of lines
+        stacks, instructions = parseInputDay5("test.txt") if not use_real_input else parseInputDay5("real.txt")
+
+        # O(N) time where N is the number of lines for instructions
+        for instruction in instructions:
+            # Move M number of boxes from one stack to another
+            tmp_stack: List[str] = []
+            for _ in range(instruction[2]):
+                tmp_stack.append(stacks[instruction[0] - 1].pop())
+            if second_problem:
+                # In second problem, just reverse the stack we are popping from
+                tmp_stack = tmp_stack[::-1]
+            stacks[instruction[1] - 1].extend(tmp_stack)
+        # Finally, pop the top of each stack to our res:
+        for stack in stacks:
+            res += stack.pop()
+        return res
+
+run = Solution()
+print(run.stack_crate_operator(True, False))
+print(run.stack_crate_operator(True, True))
--- utility.py
+++ utility.py
@@ -1,10 +1,11 @@
-from typing import Any
-
+from typing import Dict, Any, List, Tuple
+from collections import defaultdict, deque
+import math
 
 def parseInput(pathToFile: str) -> Any:
     """
     Helper function to parse files
-    :param pathToFile: Patth to input file
+    :param pathToFile: Path to input file
     :return: Any. Parsed data
     """
     res = ""
@@ -12,3 +13,41 @@ def parseInput(pathToFile: str) -> Any:
         for line in f:
             res += line
     return res
+
+
+def parseInputDay5(pathToFile: str) -> (List[List[str]], List[Tuple[int]]):
+    """
+    Helper function to parse stacks for
+    day5 problem
+    :param pathToFile: Path to input file
+    :return: Any. Parsed data
+    """
+    initial_stack: Dict[int, deque] = defaultdict(deque)
+    instructions: List[Tuple[int]] = []
+    res = ""
+    with open(pathToFile, 'r') as f:
+
+        # Handle initial stack
+        for line in f:
+            if line == "\n":
+                break
+            line_len = len(line)
+            for i in range(1, line_len, 4):
+                # We reached the line that shows stack number idx
+                if line[i].isdigit():
+                    break
+                if line[i] == ' ':
+                    continue
+                initial_stack[math.ceil(i/4)].appendleft(line[i])
+        stack_in_list = [[]] * len(initial_stack)
+        for idx, stack in initial_stack.items():
+            stack_in_list[idx-1] = stack
+
+
+        # Handle instructions:
+        for line in f:
+            if line == "\n" or "move" not in line:
+                continue
+            instruction = line.split(" ")
+            instructions.append((int(instruction[3]), int(instruction[5]), int(instruction[1])))
+        return stack_in_list, instructions
